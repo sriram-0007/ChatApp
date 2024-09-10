5adbadf581a537c84ed7c80b3b8e54dd249051d3
sriramyadav <sriramyadav885@gmail.com>
2024-09-10 15:14:48 +0530

    Allowed external host to  aceess

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Allow connections from external hosts
+    port: process.env.PORT ? parseInt(process.env.PORT, 10) : 5173, // Use the port specified in environment variables, default to 5173
+  }
 })
